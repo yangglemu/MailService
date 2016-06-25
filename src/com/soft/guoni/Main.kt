09b30eb2f17505a44e7d356af9d52426429b737b
yangglemu <yangglemu@qq.com>
2016-06-25 15:49:00 +0800

    by yangglemu

--- src/com/soft/guoni/Main.kt
+++ src/com/soft/guoni/Main.kt
@@ -1,12 +1,15 @@
 package com.soft.guoni
 
+import javax.swing.SwingUtilities
+
 /**
  * Created by 123456 on 2016/6/20.
  */
 
-val hour = 1000 * 60 * 60
 
 fun main(args: Array<String>) {
     Class.forName("com.mysql.jdbc.Driver")
-    MyFrame("邮件定时发送清理").isVisible = true
+    val f = MyFrame("邮件定时发送清理")
+    f.isVisible = true
+    SwingUtilities.invokeLater { f.isVisible = false }
 }
